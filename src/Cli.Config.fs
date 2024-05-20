2681061512195fc2e777166bc47bc07001fdda20
Romain BERTHON <>
2024-05-20 21:09:44 +0200

    feat: add command line to init repository on active directory

---
+++ src/Cli.Config.fs
@@ -0,0 +1,34 @@
+﻿module SyncBackup.Cli.Config
+
+open System
+open Argu
+
+type Commands =
+    | [<CliPrefix(CliPrefix.None); AltCommandLine("init")>] Init_Repository of ParseResults<NoOption>
+with
+    interface IArgParserTemplate with
+        member this.Usage =
+            match this with
+            | Init_Repository _ -> "Init current directory as directory to sync for backups"
+
+and NoOption =
+    | [<Hidden>] NoOption
+with
+    interface IArgParserTemplate with
+        member this.Usage =
+            match this with
+            | NoOption -> "NoOption"
+
+let runCommand (parser: ArgumentParser<Commands>) argv =
+    let commandInfra: SyncBackup.Commands.Config.Infra = {
+        InitConfig = SyncBackup.Infra.Config.init
+    }
+
+    let results = parser.ParseCommandLine(inputs = argv, raiseOnUsage = true)
+    match results.GetSubCommand() with
+    | Init_Repository _ ->
+        let currentDirectory = Environment.CurrentDirectory
+        SyncBackup.Commands.Config.Init.run commandInfra currentDirectory
+        |> function
+            | Ok () -> "Repository initialized"
+            | Error error -> $"An error occured: {error}"
--- src/Program.fs
+++ src/Program.fs
@@ -1,2 +1,17 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open Argu
+open SyncBackup.Cli.Config
+
+let private succeedExit = 0
+
+let logger = printfn "%s"
+
+[<EntryPoint>]
+let main argv =
+    try
+        let parser = ArgumentParser.Create<Commands>()
+        runCommand parser argv
+        |> logger
+    with
+        | e -> logger (e.ToString())
+
+    succeedExit
