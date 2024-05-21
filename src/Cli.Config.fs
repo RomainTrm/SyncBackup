c600dc9e0fa3deb52c6932715cbada025e6a7f6d
Romain BERTHON <>
2024-05-21 20:04:21 +0200

    feat: add CLI command to add aliases

--- src/Cli.Config.fs
+++ src/Cli.Config.fs
@@ -3,21 +3,59 @@
 open System
 open Argu
 
-type Commands =
-    | [<CliPrefix(CliPrefix.None); AltCommandLine("init")>] Init_Repository of ParseResults<NoOption>
-with
-    interface IArgParserTemplate with
-        member this.Usage =
-            match this with
-            | Init_Repository _ -> "Init current directory as directory to sync for backups"
+module ConfigInit =
+    type Init =
+        | [<Hidden>] NoOption
+    with
+        interface IArgParserTemplate with
+            member this.Usage =
+                match this with
+                | NoOption -> "NoOption"
 
-and NoOption =
-    | [<Hidden>] NoOption
+    let runCommand commandInfra currentDirectory =
+        SyncBackup.Commands.Config.Init.run commandInfra currentDirectory
+        |> function
+            | Ok () -> "Repository initialized"
+            | Error error -> $"An error occured: {error}"
+
+module Aliases =
+    type Alias =
+        | Add of ParseResults<AddAlias>
+    with
+        interface IArgParserTemplate with
+            member this.Usage =
+                match this with
+                | Add _ -> "Add a new alias to the repository."
+    and AddAlias =
+        | [<Mandatory>] Name of string
+        | [<Mandatory>] Path of string
+    with
+        interface IArgParserTemplate with
+            member this.Usage =
+                match this with
+                | Name _ -> "The name of the alias, it will be used as the directory's name on the backup."
+                | Path _ -> "The path to the directory to backup."
+
+    let runCommand commandInfra currentDirectory (command: ParseResults<Alias>) =
+        match command.GetSubCommand() with
+        | Add options ->
+            let name = options.GetResult <@ AddAlias.Name @>
+            let path = options.GetResult <@ AddAlias.Path @>
+            ({ Name = name; Path = path }: SyncBackup.Domain.Dsl.Alias)
+            |> SyncBackup.Commands.Config.Alias.add commandInfra currentDirectory
+            |> function
+                | Ok () -> "Alias added."
+                | Error error -> $"An error occured: {error}"
+
+type Commands =
+    | [<CliPrefix(CliPrefix.None)>] Init of ParseResults<ConfigInit.Init>
+    | [<CliPrefix(CliPrefix.None)>] Alias of ParseResults<Aliases.Alias>
 with
     interface IArgParserTemplate with
         member this.Usage =
             match this with
-            | NoOption -> "NoOption"
+            | Init _ -> "Init the current directory as a source directory to sync with backups."
+            | Alias _ -> "Manage aliases (pointers to directories outside the repository's directory), only available for the source directory."
 
 let runCommand (parser: ArgumentParser<Commands>) argv =
     let commandInfra: SyncBackup.Commands.Config.Infra = {
@@ -28,10 +66,8 @@ let runCommand (parser: ArgumentParser<Commands>) argv =
     }
 
     let results = parser.ParseCommandLine(inputs = argv, raiseOnUsage = true)
+    let currentDirectory = Environment.CurrentDirectory
+
     match results.GetSubCommand() with
-    | Init_Repository _ ->
-        let currentDirectory = Environment.CurrentDirectory
-        SyncBackup.Commands.Config.Init.run commandInfra currentDirectory
-        |> function
-            | Ok () -> "Repository initialized"
-            | Error error -> $"An error occured: {error}"
+    | Init _ -> ConfigInit.runCommand commandInfra currentDirectory
+    | Alias command -> Aliases.runCommand commandInfra currentDirectory command
