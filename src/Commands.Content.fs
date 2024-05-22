2cba8676c10a731b86487ef010c47bbb6b2558ab
Romain BERTHON <>
2024-05-22 18:01:16 +0200

    feat: print content

---
+++ src/Commands.Content.fs
@@ -0,0 +1,28 @@
+﻿module SyncBackup.Commands.Content
+
+open Microsoft.FSharp.Core
+open SyncBackup.Domain.Dsl
+
+type Infra = {
+    LoadAliases: unit -> Result<Alias list, string>
+    LoadFiles: Alias list -> Content list
+}
+
+let rec private printContent = function
+    | Directory { RelativePath = Source path; Content = content } ->
+        [
+            $"{path} (directory)"
+            yield! content |> List.collect printContent
+        ]
+    | Directory { RelativePath = Alias path; Content = content } ->
+        [
+            $"{path} (directory, alias)"
+            yield! content |> List.collect printContent
+        ]
+    | File { RelativePath = Source path } -> [ $"{path} (file)" ]
+    | File { RelativePath = Alias path } -> [ $"{path} (file, alias)" ]
+
+let scanRepositoryContent (infra: Infra) () =
+    infra.LoadAliases ()
+    |> Result.map infra.LoadFiles
+    |> Result.map (List.collect printContent)
---
+++ tests/Commands.Content.fs
@@ -0,0 +1,45 @@
+﻿module SyncBackup.Tests.Commands.Content
+
+open Xunit
+open Swensen.Unquote
+open SyncBackup.Domain.Dsl
+open SyncBackup.Commands.Content
+
+module ``scanRepositoryContent should`` =
+    let aliases : Alias list = [
+        { Name = "alias1"; Path = "path1" }
+        { Name = "alias2"; Path = "path2" }
+    ]
+
+    let content = [
+        Directory { Name = "dir"; RelativePath = Source "path1"; Content = [] }
+        File { Name = "file"; RelativePath = Source "path2" }
+        Directory { Name = "dir2"; RelativePath = Source "path3"; Content = [
+            File { Name = "file"; RelativePath = Source "path3\\path4" }
+            Directory { Name = "dir"; RelativePath = Source "path3\\path5"; Content = [
+                File { Name = "file"; RelativePath = Source "path3\\path5\\path6" }
+            ] }
+        ] }
+        File { Name = "file"; RelativePath = Alias "path7" }
+    ]
+
+    [<Fact>]
+    let ``retrieve content for repository and format it`` () =
+        let infra = {
+            LoadAliases = fun () -> Ok aliases
+            LoadFiles = fun a ->
+                test <@ a = aliases @>
+                content
+        }
+
+        let result = scanRepositoryContent infra ()
+        let expected = [
+            "path1 (directory)"
+            "path2 (file)"
+            "path3 (directory)"
+            "path3\\path4 (file)"
+            "path3\\path5 (directory)"
+            "path3\\path5\\path6 (file)"
+            "path7 (file, alias)"
+        ]
+        test <@ result = Ok expected @>
