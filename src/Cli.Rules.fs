c3525e25125a63e3eee1ea5e3843ed6282cc8dba
Romain BERTHON <Romain BERTHON>
2024-05-25 14:46:42 +0200

    feat: implement rule conflict resolution

--- src/Cli.Rules.fs
+++ src/Cli.Rules.fs
@@ -1,10 +1,27 @@
 ﻿module SyncBackup.Cli.Rules
 
+open System
 open Argu
 open SyncBackup.Domain
 
 let solveConflict logger (rule1: Dsl.Rule) (rule2: Dsl.Rule) : Result<Dsl.Rule, string> =
-    Error "Not implemented"
+    let path =
+        match rule1.Path with
+        | Dsl.Alias path -> path
+        | Dsl.Source path -> path
+
+    logger $"Rules conflict for path \"{path}\":"
+    logger $"1 {Dsl.SyncRules.getValue rule1.SyncRule}"
+    logger $"2 {Dsl.SyncRules.getValue rule2.SyncRule}"
+
+    let rec solveConflict' () =
+        logger "Choose rule to keep (1/2/q):"
+        match Console.ReadLine () with
+        | "1" -> Ok rule1
+        | "2" -> Ok rule2
+        | "q" -> Error "Add rule aborted"
+        | _ -> solveConflict' ()
+    solveConflict' ()
 
 type Alias =
     | Add of Rule: string * Path: string
