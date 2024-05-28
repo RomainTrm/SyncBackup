c5efc9d79b8e4ba3e163812bbe09e2b4bf2d1da2
Romain BERTHON <>
2024-05-28 21:06:33 +0200

    feat: add CLI command to init backup repository

--- src/Cli.Init.fs
+++ src/Cli.Init.fs
@@ -2,14 +2,20 @@
 
 open Argu
 
-type Init = | Source
+type Init =
+    | Source
+    | Backup
 with
     interface IArgParserTemplate with
         member this.Usage =
             match this with
-            | Source -> "Initialize the current directory as a source directory to synchronize with backups."
+            | Source -> "Initialize the current directory as a source repository to synchronize with backups."
+            | Backup -> "Initialize the current directory as a backup repository."
 
 let runCommand commandInfra = function
     | Source ->
         SyncBackup.Commands.Config.Init.source commandInfra
+        |> Result.map (fun () -> "Repository initialized")
+    | Backup ->
+        SyncBackup.Commands.Config.Init.backup commandInfra
         |> Result.map (fun () -> "Repository initialized")
