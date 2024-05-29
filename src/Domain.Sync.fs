a27891b99e85d2fff15ec8cf68d78038edee67b0
Romain BERTHON <>
2024-05-29 16:44:39 +0200

    feat: compute sync actions for empty backup

---
+++ src/Domain.Sync.fs
@@ -0,0 +1,41 @@
+﻿module SyncBackup.Domain.Sync
+
+open SyncBackup.Domain.Dsl
+
+type SyncInstruction =
+    | Add of RelativePath
+    | Replace of RelativePath
+    | Delete of RelativePath
+
+type private SourceRule =
+    | Include
+    | Exclude
+
+let private spreadSourceRules (rules: Rule list) =
+    let computeRule (lastRule: SourceRule) = function
+        | Dsl.SyncRules.NoRule -> lastRule
+        | Dsl.SyncRules.Include -> Include
+        | Dsl.SyncRules.Exclude -> Exclude
+        |  _ -> failwith "not supposed to happen"
+
+    let rec spreadSourceRules' lastRule acc = function
+        | [] -> acc
+        | rule: Rule::rules ->
+            let subPathRules, others = rules |> List.partition (fun possibleChild -> RelativePath.contains possibleChild.Path rule.Path)
+            let appliedRule = computeRule lastRule rule.SyncRule
+            let childRules = spreadSourceRules' appliedRule [] subPathRules
+            let otherRules = spreadSourceRules' lastRule [] others
+            acc@[rule.Path, appliedRule]@childRules@otherRules
+
+    rules
+    |> List.sortBy _.Path.Value
+    |> spreadSourceRules' Include []
+
+let synchronize (sourceRules: Rule list) (backupRules: Rule list) =
+    let sourceRules = spreadSourceRules sourceRules
+    sourceRules
+    |> List.collect (fun (path, syncRule) ->
+        match syncRule with
+        | Exclude -> []
+        | Include -> [Add path]
+    )
---
+++ tests/Domain.Sync.fs
@@ -0,0 +1,41 @@
+﻿module SyncBackup.Tests.Domain.Sync
+
+open SyncBackup.Domain.Dsl
+open Xunit
+open Swensen.Unquote
+open SyncBackup.Domain.Sync
+
+module ``synchronize should`` =
+    let d1 = { Type = Alias; Value = "d1"; ContentType = Directory }
+    let d2 = { Type = Alias; Value = "d2"; ContentType = Directory }
+    let d3 = { Type = Alias; Value = "d3"; ContentType = Directory }
+    let d2f1 = { Type = Alias; Value = "d2/f1"; ContentType = File }
+    let d2f2 = { Type = Alias; Value = "d2/f2"; ContentType = File }
+    let d2f3 = { Type = Alias; Value = "d2/f3"; ContentType = File }
+    let d3f1 = { Type = Alias; Value = "d3/f1"; ContentType = File }
+    let d3f2 = { Type = Alias; Value = "d3/f2"; ContentType = File }
+    let d3f3 = { Type = Alias; Value = "d3/f3"; ContentType = File }
+
+    [<Fact>]
+    let ``compute synchronize instructions when empty backup`` () =
+        let sourceRules: Rule list = [
+            { Path = d1; SyncRule = SyncRules.NoRule }
+            { Path = d2; SyncRule = SyncRules.Include }
+            { Path = d3; SyncRule = SyncRules.Exclude }
+            { Path = d2f1; SyncRule = SyncRules.NoRule }
+            { Path = d2f2; SyncRule = SyncRules.Include }
+            { Path = d2f2; SyncRule = SyncRules.Exclude }
+            { Path = d3f1; SyncRule = SyncRules.NoRule }
+            { Path = d3f2; SyncRule = SyncRules.Include }
+            { Path = d3f2; SyncRule = SyncRules.Exclude }
+        ]
+
+        let result = synchronize sourceRules []
+        let expected = [
+            Add d1
+            Add d2
+            Add d2f1
+            Add d2f2
+            Add d3f2
+        ]
+        test <@ result = expected @>
