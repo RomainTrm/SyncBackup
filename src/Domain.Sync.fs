bdef8186b04806d41cee2d2c35f0d6195104b245
Romain BERTHON <>
2024-05-29 17:26:03 +0200

    feat: compute sync actions for backup with existing content and rules

--- src/Domain.Sync.fs
+++ src/Domain.Sync.fs
@@ -18,24 +18,70 @@ let private spreadSourceRules (rules: Rule list) =
         | Dsl.SyncRules.Exclude -> Exclude
         |  _ -> failwith "not supposed to happen"
 
-    let rec spreadSourceRules' lastRule acc = function
+    let rec spreadRules lastRule acc = function
         | [] -> acc
         | rule: Rule::rules ->
             let subPathRules, others = rules |> List.partition (fun possibleChild -> RelativePath.contains possibleChild.Path rule.Path)
             let appliedRule = computeRule lastRule rule.SyncRule
-            let childRules = spreadSourceRules' appliedRule [] subPathRules
-            let otherRules = spreadSourceRules' lastRule [] others
+            let childRules = spreadRules appliedRule [] subPathRules
+            let otherRules = spreadRules lastRule [] others
             acc@[rule.Path, appliedRule]@childRules@otherRules
 
     rules
     |> List.sortBy _.Path.Value
-    |> spreadSourceRules' Include []
+    |> spreadRules Include []
+
+type private BackupRule =
+    | Save
+    | Replace
+    | NotSave
+    | NotDelete
+
+let private spreadBackupRules (rules: Rule list) =
+    let computeRule (lastRule: BackupRule) = function
+        | Dsl.SyncRules.NoRule -> lastRule
+        | Dsl.SyncRules.AlwaysReplace -> Replace
+        | Dsl.SyncRules.NotSave -> NotSave
+        | Dsl.SyncRules.NotDelete -> NotDelete
+        |  _ -> failwith "not supposed to happen"
+
+    let rec spreadRules lastRule acc = function
+        | [] -> acc
+        | rule: Rule::rules ->
+            let subPathRules, others = rules |> List.partition (fun possibleChild -> RelativePath.contains possibleChild.Path rule.Path)
+            let appliedRule = computeRule lastRule rule.SyncRule
+            let childRules = spreadRules appliedRule [] subPathRules
+            let otherRules = spreadRules lastRule [] others
+            acc@[rule.Path, appliedRule]@childRules@otherRules
+
+    rules
+    |> List.sortBy _.Path.Value
+    |> spreadRules Save []
+
+let private fullOuterJoin (sourceRules: (RelativePath * SourceRule) list) (backupRules: (RelativePath * BackupRule) list)  =
+    let sourceRulesMap = sourceRules |> List.map (fun x -> fst x, snd x) |> Map
+    let backupRulesMap = backupRules |> List.map (fun x -> fst x, snd x) |> Map
+    [
+        sourceRulesMap |> Seq.map (fun x -> x.Key, Some x.Value, backupRulesMap |> Map.tryFind x.Key)
+        backupRulesMap |> Seq.map (fun x -> x.Key, sourceRulesMap |> Map.tryFind x.Key, Some x.Value)
+    ]
+    |> Seq.concat
+    |> Seq.distinct
+    |> Seq.sortBy (fun (path, _, _) -> path.Value)
+    |> Seq.toList
 
 let synchronize (sourceRules: Rule list) (backupRules: Rule list) =
     let sourceRules = spreadSourceRules sourceRules
-    sourceRules
-    |> List.collect (fun (path, syncRule) ->
-        match syncRule with
-        | Exclude -> []
-        | Include -> [Add path]
+    let backupRules = spreadBackupRules backupRules
+    let spreadRules = fullOuterJoin sourceRules backupRules
+
+    spreadRules
+    |> List.collect (function
+        | path, Some Include, None -> [Add path]
+        | _, Some Include, Some Save -> []
+        | path, Some Include, Some Replace -> [SyncInstruction.Replace path]
+        | _, Some Include, Some NotSave -> []
+        | _, Some Exclude, None -> []
+        | _, None, Some NotDelete -> []
+        | path, None, _ -> [Delete path]
     )
--- tests/Domain.Sync.fs
+++ tests/Domain.Sync.fs
@@ -24,10 +24,10 @@ module ``synchronize should`` =
             { Path = d3; SyncRule = SyncRules.Exclude }
             { Path = d2f1; SyncRule = SyncRules.NoRule }
             { Path = d2f2; SyncRule = SyncRules.Include }
-            { Path = d2f2; SyncRule = SyncRules.Exclude }
+            { Path = d2f3; SyncRule = SyncRules.Exclude }
             { Path = d3f1; SyncRule = SyncRules.NoRule }
             { Path = d3f2; SyncRule = SyncRules.Include }
-            { Path = d3f2; SyncRule = SyncRules.Exclude }
+            { Path = d3f3; SyncRule = SyncRules.Exclude }
         ]
 
         let result = synchronize sourceRules []
@@ -37,5 +37,33 @@ module ``synchronize should`` =
             Add d2f1
             Add d2f2
             Add d3f2
+        ]
+        test <@ result = expected @>
+
+    [<Fact>]
+    let ``compute synchronize instructions when not empty backup`` () =
+        let sourceRules: Rule list = [
+            { Path = d2; SyncRule = SyncRules.NoRule }
+            { Path = d3; SyncRule = SyncRules.NoRule }
+            { Path = d2f1; SyncRule = SyncRules.NoRule }
+            { Path = d2f2; SyncRule = SyncRules.NoRule }
+            { Path = d2f3; SyncRule = SyncRules.NoRule }
+        ]
+
+        let result = synchronize sourceRules [
+            { Path = d1; SyncRule = SyncRules.NoRule }
+            { Path = d2; SyncRule = SyncRules.NoRule }
+            { Path = d3; SyncRule = SyncRules.NoRule }
+            { Path = d2f1; SyncRule = SyncRules.AlwaysReplace }
+            { Path = d2f2; SyncRule = SyncRules.AlwaysReplace }
+            { Path = d2f3; SyncRule = SyncRules.NotSave }
+            { Path = d3f1; SyncRule = SyncRules.NotDelete }
+            { Path = d3f2; SyncRule = SyncRules.NotDelete }
+            { Path = d3f3; SyncRule = SyncRules.NotDelete }
+        ]
+        let expected = [
+            Delete d1
+            Replace d2f1
+            Replace d2f2
         ]
         test <@ result = expected @>
