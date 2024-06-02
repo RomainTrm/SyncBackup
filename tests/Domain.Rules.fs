8c858e9586ce8fc3a7982692e6b608d1efc648f5
Romain BERTHON <>
2024-06-02 15:56:24 +0200

    feat: add test to compute rules for sync

---
+++ tests/Domain.Rules.fs
@@ -0,0 +1,32 @@
+﻿module SyncBackup.Tests.Domain.Rules
+
+open Xunit
+open Swensen.Unquote
+
+open SyncBackup.Domain.Dsl
+open SyncBackup.Domain.Rules
+
+module ``buildRulesForSyncing should`` =
+    let p1 = { Value = "p1"; Type = Source; ContentType = File }
+    let p2 = { Value = "p2"; Type = Source; ContentType = File }
+    let p3 = { Value = "p3"; Type = Source; ContentType = File }
+    let p4 = { Value = "p4"; Type = Source; ContentType = File }
+
+    [<Fact>]
+    let ``apply rules correctly`` () =
+        let existingElements = [p1; p2; p3; p4]
+        let setupRules = [
+            {  Path = p2; SyncRule = SyncRules.NotSave }
+            {  Path = p3; SyncRule = SyncRules.Exclude }
+            {  Path = p4; SyncRule = SyncRules.NotDelete }
+        ]
+
+        let result = buildRulesForSyncing setupRules existingElements
+
+        let expected = [
+            { Path = p1; SyncRule = SyncRules.NoRule }
+            { Path = p2; SyncRule = SyncRules.NotSave }
+            { Path = p3; SyncRule = SyncRules.Exclude }
+            { Path = p4; SyncRule = SyncRules.NotDelete }
+        ]
+        test <@ result = expected @>
